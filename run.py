8593c5fabf21b4052e739059d888a47822bac438
theta01adi <aditya.patel@thetatechnolabs.com>
2025-06-05 11:28:55 +0530

    Cors added.

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from dotenv import load_dotenv
 from flask_smorest import Api
+from flask_cors import CORS
 
 from app.api.routes import post_blp as post_blueprint
 
@@ -16,6 +17,14 @@ def create_app():
     app.config["OPENAPI_URL_PREFIX"] = "/docs"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+    cors_config = {
+        "origins": ["http://localhost:5173"],
+        "methods": ["GET", "POST"],
+        "allow_headers": ["Content-Type", "Authorization"],
+    }
+
+    CORS(app, resources={r"/*": cors_config})
 
     api = Api(app=app)
 
