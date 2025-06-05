274541b9917b2237798f60eecf3a64f4f09ffccd
theta01adi <aditya.patel@thetatechnolabs.com>
2025-06-05 11:38:21 +0530

    Verify services added : verify_register_data, verify_post_submit_data. \nWeb3 services : register_user, get_username, submit_user_cid.

---
+++ app/blockchain/verification_service.py
@@ -0,0 +1,68 @@
+from eth_account.messages import encode_defunct
+from .web3_instance import get_web3_instance, get_contract_instance
+from web3 import Web3 
+from flask_smorest import abort
+
+web3 = get_web3_instance()
+contract_instance = get_contract_instance()
+
+def verify_register_data(wallet_address, signed_message, username):
+
+    if not username or not username.strip():
+        abort(400,
+              message="Username cannot be empty !!")
+    
+    if not Web3.is_address(wallet_address):
+        abort(400,
+              message="Invalid wallet address!!")
+    
+    ORIGINAL_REGISTER_MESSAGE = "You are registering to LinkedInPost Reward Dapp !!  You agree with our terms and conditions."
+
+    try:
+        message = encode_defunct(text=ORIGINAL_REGISTER_MESSAGE)
+        signer_address = Web3().eth.account.recover_message(message, signature=signed_message)
+    except Exception as e:
+        print(e)
+        abort(400,
+              message="Invalid signed message")
+    
+    if signer_address.lower() != wallet_address.lower():
+        abort(400,
+              message="Signed message does not match wallet address")
+    
+    return True
+
+def verify_post_submit_data(user_address, post_content, post_base64, signed_message):
+
+    if not Web3.is_address(user_address):
+        abort(400,
+              message="Invalid wallet address!!")
+
+    if not post_base64 or not post_base64.strip():
+        abort(400,
+            "Image can't be empty!!" )
+        
+    if not post_content or not post_content.strip():
+        abort(
+            400,
+            "Post content can't be empty!!"
+        )
+
+    ORIGINAL_POST_SUBMIT_MESSAGE = "You are submiting your linkedin post screenshot and post content to LinkedInPost Reward Dapp !!"
+
+    try:
+        message = encode_defunct(text=ORIGINAL_POST_SUBMIT_MESSAGE)
+        signer_address = Web3().eth.account.recover_message(message, signature=signed_message)
+    except Exception as e:
+        print(e)
+        abort(400,
+              message="Invalid signed message")
+    
+    if signer_address.lower() != user_address.lower():
+        abort(400,
+              message="Signed message does not match wallet address")
+    
+    return True
+    
+
+
---
+++ app/blockchain/web3_services.py
@@ -0,0 +1,88 @@
+from .web3_instance import get_web3_instance, get_contract_instance
+from .web3_config import OWNER_PRIVATE_KEY, OWNER_PUBLIC_ADDRESS
+from flask import jsonify
+from web3 import Web3
+from flask_smorest import abort
+from web3.exceptions import ContractLogicError
+
+web3 = get_web3_instance()
+contract_instance = get_contract_instance()
+
+def get_username(user_address):
+
+    username = contract_instance.functions.userToName(Web3.to_checksum_address(user_address)).call()
+
+    return username
+
+def register_user(user_address, username):
+
+    try:
+        user_address = Web3.to_checksum_address(user_address)
+        txn = contract_instance.functions.register_user(user_address, username).build_transaction({
+        "from": OWNER_PUBLIC_ADDRESS,
+        "nonce": web3.eth.get_transaction_count(OWNER_PUBLIC_ADDRESS),
+        "gasPrice": web3.eth.gas_price 
+
+        })
+
+        signed_txn = web3.eth.account.sign_transaction(txn, private_key=OWNER_PRIVATE_KEY)
+
+        tx_hash = web3.eth.send_raw_transaction(signed_txn.raw_transaction)
+        print("Transaction sent:", tx_hash.hex())
+
+        # receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
+        # print("Transaction mined in block", receipt.blockNumber)
+
+        return tx_hash.hex()
+    except ContractLogicError as e:
+        error_message = str(e)
+        print(error_message)
+        abort(
+            400,
+            message="Contract execution failed"
+        )
+
+    except Exception as e:
+        print(str(e))
+        abort(
+            500,
+            message="Unexpected error : failed!!"
+        )
+
+
+def submit_user_cid(user_address, post_cid):
+
+
+    try:
+        user_address = Web3.to_checksum_address(user_address)
+        txn = contract_instance.functions.submit_cid(user_address, post_cid).build_transaction({
+        "from": OWNER_PUBLIC_ADDRESS,
+        "nonce": web3.eth.get_transaction_count(OWNER_PUBLIC_ADDRESS),
+        "gasPrice": web3.eth.gas_price 
+
+        })
+
+        signed_txn = web3.eth.account.sign_transaction(txn, private_key=OWNER_PRIVATE_KEY)
+
+        tx_hash = web3.eth.send_raw_transaction(signed_txn.raw_transaction)
+        print("Transaction sent:", tx_hash.hex())
+
+        # receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
+        # print("Transaction mined in block", receipt.blockNumber)
+
+        return tx_hash.hex()
+    except ContractLogicError as e:
+        error_message = str(e).split(': ')[1]
+        print(error_message)
+
+        abort(
+            400,
+            message=f"{error_message}"
+        )
+
+    except Exception as e:
+        print(str(e))
+        abort(
+            500,
+            message="Unexpected error : failed!!"
+        )
